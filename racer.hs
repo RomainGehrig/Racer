bd3b57523ba3f9aebc79f6f1c35cb76d3fb7b85b
Romain Gehrig <romain.gehrig@gmail.com>
2014-08-08 05:06:47 +0200

    before rework of Player

--- racer.hs
+++ racer.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.Maybe
+import Data.Char
 
 type Vec2D = (Int, Int)
 
@@ -9,6 +10,7 @@ type Velocity = Vec2D
 type Circuit = [[Tile]]
 type Tile = Char
 type Move = Vec2D
+type Player = (Vec2D, Tile)
 
 data TileType = Obstacle | Road deriving (Show, Eq) 
 -- data Move = NW | N | NE |
@@ -47,19 +49,82 @@ v1 `subVec` v2@(x,y) = addVec v1 (-x,-y)
 move :: Position -> Velocity -> Position
 move = addVec
 
+possiblePos :: Position -> Velocity -> [Position]
+possiblePos pos vel = [ addVec nextPos (dx,dy) | dy <- [-1..1], dx <- [-1..1]]
+    where nextPos = addVec pos vel
+
 validPos :: Circuit -> Position -> Bool
 validPos c pos = (getTileType $ getTile c pos) == Road
 
-possibleMoves :: Circuit -> Position -> [Move]
-possibleMoves c pos = [ (x,y) | x <- [-1,0,1], y <- [-1,0,1], validMove pos (x,y)]
-    where validMove pos (x,y) = (getTileType . getTile c . move pos) (x,y) == Road 
+-- possibleMoves :: Circuit -> Position -> [Move]
+-- possibleMoves c pos = [ (x,y) | x <- [-1,0,1], y <- [-1,0,1], validMove pos (x,y)]
+--     where validMove pos (x,y) = (getTileType . getTile c . move pos) (x,y) == Road 
 
-possibleVelocities :: Velocity -> Circuit -> Position -> [Velocity]
-possibleVelocities vel c pos = map (addVec vel) (possibleMoves c nextPos)
-    where nextPos = addVec vel pos
+-- possibleVelocities :: Velocity -> Circuit -> Position -> [Velocity]
+-- possibleVelocities vel c pos = map (addVec vel) (possibleMoves c nextPos)
+--     where nextPos = addVec vel pos
 
 solveRace :: Race -> Maybe [Move]
 solveRace r@(p, v, c) = Nothing
 
 circuitFind :: Circuit -> Tile -> Maybe Position
-circuitFind c t = listToMaybe [ (x,y) | (y, line) <- zip [0..] c, x <- elemIndices t line ]
+circuitFind = (listToMaybe .) . circuitFinds
+
+circuitFinds :: Circuit -> Tile -> [Position]
+circuitFinds c t = [ (x,y) | (y, line) <- zip [0..] c, x <- elemIndices t line ]
+
+-- TODO : make it less naive
+displayRace :: Circuit -> [Player] -> String
+displayRace c [] = printCircuit c
+displayRace c (p:ps) = displayRace (showPlayer c p) ps
+
+printCircuit :: Circuit -> String
+printCircuit = unlines
+
+replaceCircuit :: Circuit -> Position -> Tile -> Circuit
+replaceCircuit c pos@(x,y) t = (take y c) ++ 
+                               ((replaceCircuit' (c !! y) x t):[]) ++ 
+                               (drop (y+1) c)
+
+replaceCircuit' :: [Tile] -> Int -> Tile -> [Tile]
+replaceCircuit' l@(x:xs) index tile
+    | l == [] = []
+    | index == 0 = tile : xs
+    | otherwise  =    x : (replaceCircuit' xs (index-1) tile)
+
+showPlayer :: Circuit -> Player -> Circuit
+showPlayer c p@(pos,t) = replaceCircuit c pos t
+
+showMoves :: Circuit -> [Position] -> Circuit
+showMoves c poss = displayTiles c tiles
+    where tiles = [(intToDigit n, pos) | (n, pos) <- zip [1..] poss, validPos c pos]
+          displayTiles c ts = foldl (\ c (t,pos) -> replaceCircuit c pos t) c ts
+
+showPlayerTurn :: Circuit -> Player -> Velocity -> Circuit
+showPlayerTurn c p@(pos,_) v = showPlayer (showMoves c (possiblePos pos v)) p
+
+main :: IO ()
+main = do
+    let players = ((1,1),'*'):[]
+    putStr $ displayRace basicCircuit players
+
+handlePlayerTurn :: Circuit -> Player -> Velocity -> Bool
+handlePlayerTurn c p v = do
+    pressedKey <- getChar
+    case safeDigitToInt pressedKey of
+        Just n -> putStrLn $ "Works "++(pressedKey:[])
+        Nothing -> putStrLn "WRONG"
+    where safeDigitToInt x = if x `elem` ['1'..'9'] then Just (digitToInt x) else Nothing
+
+-- isRaceDone :: Circuit -> [Player] -> Maybe Bool
+-- isRaceDone c ps = circuitFind c 'e' >>= \ endPos ->  Just $ any (\ p@(pos,_) -> pos == endPos) ps
+
+isRaceDone :: Circuit -> [Player] -> Bool
+isRaceDone c ps = case circuitFind c 'e' of
+                     Nothing -> False
+                     Just endPos -> any (\ p@(pos,_) -> pos == endPos) ps
+
+bp :: Player
+bp = ((1,1),'*')
+bc :: Circuit
+bc = basicCircuit
